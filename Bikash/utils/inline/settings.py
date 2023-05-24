0bed9bc64cc8f1f9f87b29f9187c04f6d48f5910
massom0097773 <132870020+massom0097773@users.noreply.github.com>
2023-05-24 18:55:35 +0530

    Update settings.py

--- Bikash/utils/inline/settings.py
+++ Bikash/utils/inline/settings.py
@@ -24,7 +24,7 @@ def setting_markup(_):
         ],
         [           
             InlineKeyboardButton(
-                text="📱 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 📱", url=f"https://youtube.com/@BikashGadgetsTech"
+                text="📱 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 📱", url=f"https://t.me/about_meeBacha"
             ),
         ],
         [
