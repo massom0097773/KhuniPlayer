c74d672a210b14687bb332addf11d114d8925588
massom0097773 <132870020+massom0097773@users.noreply.github.com>
2023-05-31 01:17:52 +0530

    Update playlist.py

--- Bikash/utils/inline/playlist.py
+++ Bikash/utils/inline/playlist.py
@@ -14,7 +14,7 @@ def botplaylist_markup(_):
         ],
         [           
             InlineKeyboardButton(
-                text="📱 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 📱", url=f"https://t.me/about_meeBacha"
+                text="📱 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 📱", url=f"https://t.me/about_meeBachaa"
             ),
         ],
         [
@@ -48,7 +48,7 @@ def top_play_markup(_):
         ],
         [           
             InlineKeyboardButton(
-                text="📱 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 📱", url=f"https://t.me/about_meeBacha"
+                text="📱 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 📱", url=f"https://t.me/about_meeBachaa"
             ),
         ],
         [
