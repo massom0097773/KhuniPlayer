9c02cafa8b53f9f8dd324d399b4cf379451412ef
massom0097773 <132870020+massom0097773@users.noreply.github.com>
2023-05-31 01:53:49 +0530

    Update donate.py

--- plugins/extra/donate.py
+++ plugins/extra/donate.py
@@ -12,7 +12,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def donate(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://te.legra.ph/file/9c816d97efbbfc8a032dd.jpg",
-        caption=f"""🥀 𝐂𝐥𝐢𝐜𝐤 𝐁𝐞𝐥𝐨𝐰 𝐁𝐮𝐭𝐭𝐨𝐧 𝐅𝐨𝐫 𝐃𝐨𝐧𝐚𝐭𝐞 & 𝐂𝐥𝐢𝐜𝐤 𝐁𝐞𝐥𝐨𝐰 𝐁𝐢𝐤𝐚𝐬𝐡 𝐨𝐫 𝐀𝐝𝐢𝐭𝐲𝐚 𝐅𝐨𝐫 𝐐𝐫 𝐂𝐨𝐝𝐞, 𝐈𝐟 𝐘𝐨𝐮 𝐖𝐚𝐧𝐭 𝐏𝐫𝐨𝐦𝐨𝐭𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬 𝐎𝐫 𝐎𝐭𝐡𝐞𝐫𝐬 𝐋𝐢𝐧𝐤 𝐓𝐡𝐞𝐧 [𝐈𝐧𝐧𝐨𝐜𝐞𝐧𝐭](https://t.me/ll_Innocent_Music_lXl_Bot) & 𝐂𝐥𝐢𝐜𝐤 𝐎𝐭𝐡𝐞𝐫𝐬 𝐁𝐮𝐭𝐭𝐨𝐧 & 𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐎𝐫 𝐆𝐫𝐨𝐮𝐩.. 🥀 [𝐈𝐧𝐧𝐨𝐜𝐞𝐧𝐭](https://t.me/about_meeBacha)..""",
+        caption=f"""🥀 𝐂𝐥𝐢𝐜𝐤 𝐁𝐞𝐥𝐨𝐰 𝐁𝐮𝐭𝐭𝐨𝐧 𝐅𝐨𝐫 𝐃𝐨𝐧𝐚𝐭𝐞 & 𝐂𝐥𝐢𝐜𝐤 𝐁𝐞𝐥𝐨𝐰 Innocent  𝐨𝐫 Massom 𝐅𝐨𝐫 𝐐𝐫 𝐂𝐨𝐝𝐞, 𝐈𝐟 𝐘𝐨𝐮 𝐖𝐚𝐧𝐭 𝐏𝐫𝐨𝐦𝐨𝐭𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬 𝐎𝐫 𝐎𝐭𝐡𝐞𝐫𝐬 𝐋𝐢𝐧𝐤 𝐓𝐡𝐞𝐧 [𝐈𝐧𝐧𝐨𝐜𝐞𝐧𝐭](https://t.me/ll_Innocent_Music_lXl_Bot) & 𝐂𝐥𝐢𝐜𝐤 𝐎𝐭𝐡𝐞𝐫𝐬 𝐁𝐮𝐭𝐭𝐨𝐧 & 𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐎𝐫 𝐆𝐫𝐨𝐮𝐩.. 🥀 [𝐈𝐧𝐧𝐨𝐜𝐞𝐧𝐭](https://t.me/about_meeBachaa)..""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -21,14 +21,14 @@ async def donate(client: Client, message: Message):
             ],          
             [
                     InlineKeyboardButton(
-                        "🥀 𝘼𝘼𝙎𝙃𝙑𝙄 🥀", url=f"https://t.me/Nikal_beee")
+                        "🥀 MASSOM 🥀", url=f"https://t.me/innocentpapabotle")
                 ],
                 [
                     InlineKeyboardButton(
                         "🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"https://t.me/innocentop"
                     ),
                     InlineKeyboardButton(
-                        "🥀 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🥀", url=f"https://t.me/about_meeBacha")
+                        "🥀 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🥀", url=f"https://t.me/about_meeBachaa")
                 ]
             ]
         ),
